a6024a83e0dcde0717e77d622fb9e9e6d84ec415
7 <fengzilijie@qq.com>
2019-05-08 11:49:25 +0800

    +

--- src/service.ts
+++ src/service.ts
@@ -19,6 +19,14 @@ export class Service {
     }
   }
 
+  /**
+   *
+   * @param param.type 'pv' 页面浏览, 'ck' 点击事件, 'expo' 曝光, 'ev' 通用事件
+   * @param param.url 页面route
+   * @param param.uid 事件handler/eventname，曝光selector
+   */
+  log({ type, url, uid, extra }) {}
+
   static install(obj: any, options: any): void {
     // Not obj.prototype.$service
     obj.$service = new Service(options);
